d09848af1014afadb59e3030821874bdf483410a
Evan A Vaughan <41272360+evanavaughan@users.noreply.github.com>
2018-10-30 09:47:43 -0400

    Update airline.py

--- week-2/object-relations/lib/airline.py
+++ week-2/object-relations/lib/airline.py
@@ -1,2 +1,12 @@
 class Airline():
-    pass
+    def get_name(self):
+        return self._name
+
+    def set_name(self):
+        self._name = name
+
+    def get_year(self):
+        return self._year
+
+    def set_year(self):
+        self._year = year
